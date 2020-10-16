408661d53dbf26c9f1fb83cc85a50b63576a1242
Il-Pittore <antoniozarrilli03@gmail.com>
2020-10-16 19:49:24 +0200

    implemantazione orario, saluti, stop, insulto

--- Python/AssistenteVocale.py
+++ Python/AssistenteVocale.py
@@ -2,11 +2,19 @@ from playsound import playsound
 import pyttsx3
 import os
 import speech_recognition as sp
+import datetime
+import shlex
 from random import randrange
 import pyaudio
 
 saluti_in = ["ciao", "ciao luna", "ehi", "eccomi"]
 saluti_out = ["ehi", "ciao", "sono felice che tu mi abbia salutato"]
+stop = ["stop", "basta", "stai zitta"]
+luna = ["luna"]
+time_clock = ["che ore sono", "orario", "ora"]
+insulto_in = ["stai zitta puttana", "stai zitta troia", "puttana", "troia", "ritardata", "aborto"]
+insulto_out = ["hai ragione", "non è corretto nei miei confronti"]
+
 
 def speak(text):
     engine = pyttsx3.init()
@@ -43,17 +51,32 @@ def get_audio():
             print("errore")
     return result
 
-
+muta = 0
 while True:
+    print("sono in ascolto...")
     text_in = get_audio()
+    #text_in = input()
+    print("sto elaborando...")
     text_in = text_in.lower()
     text_out = text_in
     if text_in != None and text_in != "":
         if text_in in saluti_in:
             ran = randrange(len(saluti_out))
             text_out = saluti_out[ran]
+        elif text_in in stop:
+            text_out = "Ok a presto"
+            muta = 1
+            speak(text_out)
+        elif text_in in luna:
+            text_out = "Dimmi"
+            muta = 0
+        elif text_in in time_clock:
+            dataOra = datetime.datetime.now()
+            dataOra = str(dataOra)
+            ora = dataOra.split()
+            text_out = "sono le: " + ora[1]
         else:
-            text_out = "Non conosco queta parola, scusa."
-        speak(text_out)
-
-#text = input()
+            text_out = "Non conosco questa parola, scusa."
+        if muta == 0:
+            speak(text_out)
+            print("-----ho risposto-----")
